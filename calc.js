e0b141e3d2d07531a7eff4cd728aa3191eb0bf4a
Patrick Oser <patrick.oser148@gmail.com>
2023-02-06 18:55:07 -0500

    Small changes.

--- calc.js
+++ calc.js
@@ -1,22 +1,22 @@
-// Performs addition on the values it is given
+// Performs addition on the values it is given.
 
 function add() {
 
 }
 
-// Performs subtraction on the values it is given
+// Performs subtraction on the values it is given.
 
 function subtract() {
 
 }
 
-// Multiplies on the values it is given
+// Multiplies on the values it is given.
 
 function multiply() {
 
 }
 
-// Divides any of the values it is given
+// Divides any of the values it is given.
 
 function divide() {
 
@@ -26,5 +26,12 @@ function divide() {
 // of the numbers given.
 
 function operate() {
+
+}
+
+// Clears all values being stored and wipes the display clean so the user
+// can start over.
+
+function clear() {
 
 }
