f8f0caf201ecf34b3e832ff867a81f1b701d2baf
Patrick Oser <patrick.oser148@gmail.com>
2023-02-06 18:52:56 -0500

    Created shell for functions.

--- calc.js
+++ calc.js
@@ -1,1 +1,30 @@
-console.log('Hello');
+// Performs addition on the values it is given
+
+function add() {
+
+}
+
+// Performs subtraction on the values it is given
+
+function subtract() {
+
+}
+
+// Multiplies on the values it is given
+
+function multiply() {
+
+}
+
+// Divides any of the values it is given
+
+function divide() {
+
+}
+
+// Calls on the functions connected to the operators to find the value
+// of the numbers given.
+
+function operate() {
+
+}
