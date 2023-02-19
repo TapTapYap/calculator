fe5b662631e97023d0c46899f5c4e4c2f48f89eb
Patrick Oser <patrick.oser148@gmail.com>
2023-02-19 11:34:33 -0500

    Added functionality to clear function and put it in constructor.

--- calc.js
+++ calc.js
@@ -2,10 +2,13 @@ class Calculator {
     constructor(previousOperandTextElement, currentOperandTextElement) {
         this.previousOperandTextElement = previousOperandTextElement;
         this.currentOperandTextElement = currentOperandTextElement;
+        this.clear();
     }
 
     clear () {
-
+        this.currentOperandTextElement = '';
+        this.previousOperandTextElement = '';
+        this.operation = undefined;
     }
 
     delete() {
